e25272f50d23b8b854c76cc8cd555e0e35791174
Nagypál Márton Péter <nagypal.marton.peter@edu.bme.hu>
2024-11-14 21:00:27 +0100

    Implement digit recognition model with Gradio interface and update requirements

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-import app as gr
+import tensorflow as tf
+import numpy as np
+from urllib.request import urlretrieve
+import gradio as gr
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
+urlretrieve("https://gr-models.s3-us-west-2.amazonaws.com/mnist-model.h5", "mnist-model.h5")
+model = tf.keras.models.load_model("mnist-model.h5")
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
-)
+def recognize_digit(image):
+    image = image.reshape(1, -1)  
+    prediction = model.predict(image).tolist()[0]
+    return {str(i): prediction[i] for i in range(10)}
 
-demo.launch()
+output_component = gr.Label(num_top_classes=5)
+
+gr.Interface(fn=recognize_digit, 
+             inputs="sketchpad", 
+             outputs=output_component,
+             title="Digit Sketchpad",
+             description="Draw a digit between 0 and 9.").launch(share=True);
